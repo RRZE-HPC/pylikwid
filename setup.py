d624eb95eb58ebf095bc080bc2e89d66c7fbf15f
Julian Hammer <julian.hammer@fau.de>
2017-04-20 12:26:20 +0200

    added supported python versions to classifiers

--- setup.py
+++ setup.py
@@ -33,6 +33,11 @@ setup(
         "Topic :: Utilities",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
     ],
     package_data = {
         "pylikwid" : ["pylikwid.c", "README.rst", "LICENSE"],
