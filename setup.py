5fa46d0927723a29015760fb96f85692b4c0f76a
Thomas Roehl <thomas.roehl@fau.de>
2018-01-02 15:44:51 +0100

    Add single variable to hold prefix of LIKWID for proper linking

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 import os, os.path, glob, re
 from distutils.core import setup, Extension
 
+LIKWID_PREFIX = "/usr/local"
+
+
 # Utility function to read the README.md file.
 # Used for the long_description.  It"s nice, because now 1) we have a top level
 # README.md file and 2) it"s easier to type in the README.md file than to put a raw
@@ -33,16 +36,16 @@ def get_highest_version(paths, lib):
     return ":"+max_lib.replace(max_path, "").strip("/")
 
 pylikwid = Extension("pylikwid",
-                    include_dirs = ["/usr/local/include"],
-                    libraries = [get_highest_version(["/usr/local/lib"], "liblikwid.so")],
+                    include_dirs = [os.path.join(LIKWID_PREFIX, "include")],
+                    libraries = [get_highest_version([os.path.join(LIKWID_PREFIX, "lib")], "liblikwid.so")],
                     #libraies = ["likwid"] links with liblikwid.so
                     #libraies = [":liblikwid.so.4.3"] links with liblikwid.so.4.3
-                    library_dirs = ["/usr/local/lib"],
+                    library_dirs = [os.path.join(LIKWID_PREFIX, "lib")],
                     sources = ["pylikwid.c"])
 
 setup(
     name = "pylikwid",
-    version = "0.2.2",
+    version = "0.2.3",
     author = "Thomas Roehl",
     author_email = "thomas.roehl@gmail.com",
     description = ("A Python module to access the function of the LIKWID library"),
