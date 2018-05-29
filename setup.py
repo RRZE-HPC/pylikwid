493d50f0f3b6e9c8998197df498a2da3eb7409f2
Thomas Roehl <thomas.roehl@fau.de>
2018-05-29 15:20:02 +0200

    Inc version

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ pylikwid = Extension("pylikwid",
 
 setup(
     name = "pylikwid",
-    version = "0.2.5",
+    version = "0.2.6",
     author = "Thomas Roehl",
     author_email = "thomas.roehl@gmail.com",
     description = ("A Python module to access the function of the LIKWID library"),
