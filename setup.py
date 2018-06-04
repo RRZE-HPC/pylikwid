9d7ea9d7a2172b173bde21b54514ff0eac2cf67f
Thomas Roehl <thomas.roehl@fau.de>
2018-06-04 12:15:43 +0200

    Improved search for likwid library and include paths

--- setup.py
+++ setup.py
@@ -18,55 +18,66 @@ def get_prefix():
     print("Searching for LIKWID installation")
     for p in os.environ["PATH"].split(":"):
         cmd = "find %s/.. -type f -name \"liblikwid.so*\" 2>&1 | grep \"lib/\" | head -n1" % (p,)
-        ps = subprocess.Popen(cmd, shell=True, close_fds=True, stdout=subprocess.PIPE)
-        sout, serr = ps.communicate()
-        if sout:
-            if len(sout) > 0:
-                path = b"/".join(os.path.normpath(sout.strip()).split(b"/")[:-2])
-                break
+        try:
+            ps = subprocess.Popen(cmd, shell=True, close_fds=True, stdout=subprocess.PIPE)
+            sout, serr = ps.communicate()
+            if sout:
+                if len(sout) > 0:
+                    path = b"/".join(os.path.normpath(sout.strip()).split(b"/")[:-2])
+                    break
+        except:
+            pass
     print("Using LIKWID libray at {!s}".format(path.decode()))
     return bytes(path).decode().strip("\n").encode()
 
+def get_lib_path(prefix):
+    path = (prefix.decode()+"/lib").encode()
+    lib = "likwid"
+    cmd = "find %s -type f -name \"liblikwid.so*\" 2>&1 | sort | tail -n 1" % (prefix.decode(),)
+    try:
+        ps = subprocess.Popen(cmd, shell=True, close_fds=True, stdout=subprocess.PIPE)
+        sout, serr = ps.communicate()
+        
+        if sout:
+            inp = sout.decode().strip("\n").encode()
+            path = os.path.dirname(inp)
+            lib = b":"+os.path.basename(inp)
+    except:
+        pass
+    return path, lib
 
-def get_highest_version(paths, lib):
-    max_lib = ""
-    max_path = ""
-    for p in paths:
-        if os.path.exists(p):
-            libs = glob.glob(os.path.join(p, lib)+b"*")
-            for l in libs:
-                if ver_regex.search(l):
-                    if len(l) > len(max_lib):
-                        max_lib = l
-                        max_path = p
-                        continue
-                    if len(max_lib) > 0 and len(l) == len(max_lib):
-                        m = ver_regex.search(l)
-                        max_m = ver_regex.search(max_lib)
-                        for i in range(len(max_m.groups())):
-                            if int(m.group(i+1)) > int(max_m.group(i+1)):
-                                max_lib = l
-                                max_path = p
-                                break
-    return (b":"+max_lib.replace(max_path, b"").strip(b"/")).decode()
+def get_inc_path(prefix):
+    path = (prefix.decode()+"/include").encode()
+    cmd = "find %s -type f -name \"likwid.h\" 2>&1 | sort | tail -n 1" % (prefix.decode(),)
+    try:
+        ps = subprocess.Popen(cmd, shell=True, close_fds=True, stdout=subprocess.PIPE)
+        sout, serr = ps.communicate()
+        if sout:
+            path = os.path.dirname(sout.decode().strip("\n").encode())
+    except:
+        pass
+    return path
+
 
 LIKWID_PREFIX = get_prefix()
 if not LIKWID_PREFIX:
     LIKWID_PREFIX = str(DEF_LIKWID_PREFIX)
+LIKWID_LIBPATH, LIKWID_LIB = get_lib_path(LIKWID_PREFIX)
+LIKWID_INCPATH = get_inc_path(LIKWID_PREFIX)
 
 pylikwid = Extension("pylikwid",
-                    include_dirs = [os.path.join(LIKWID_PREFIX, b"include").decode()],
-                    libraries = [get_highest_version([os.path.join(LIKWID_PREFIX, b"lib")], b"liblikwid.so")],
+                    include_dirs = [LIKWID_INCPATH.decode()],
+                    libraries = [LIKWID_LIB.decode()],
                     #libraies = ["likwid"] links with liblikwid.so
                     #libraies = [":liblikwid.so.4.3"] links with liblikwid.so.4.3
-                    library_dirs = [os.path.join(LIKWID_PREFIX, b"lib").decode()],
+                    library_dirs = [LIKWID_LIBPATH.decode()],
                     sources = ["pylikwid.c"])
 
 setup(
     name = "pylikwid",
-    version = "0.2.6",
+    version = "0.2.7",
     author = "Thomas Roehl",
-    author_email = "thomas.roehl@gmail.com",
+    author_email = "thomas.roehl@googlemail.com",
     description = ("A Python module to access the function of the LIKWID library"),
     long_description=read("README.rst"),
     license = "GPLv2",
