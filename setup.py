adfa3b7908b9c4939c855dc0fdfbec8b93de2a60
Jonathan MERCIER <bioinfornatics@gmail.com>
2019-01-16 11:06:02 +0100

    fix issue #11

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
-import os, os.path, glob, re, subprocess
+import os, os.path, glob, re
 from distutils.core import setup, Extension
-
-DEF_LIKWID_PREFIX = "/usr/local"
 
 
 # Utility function to read the README.md file.
@@ -11,78 +9,96 @@ DEF_LIKWID_PREFIX = "/usr/local"
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
 ver_regex = re.compile(b"so.(\d+)[.]*(\d*)")
 
-def get_prefix():
+
+def find_file(path_iterator):
     path = None
-    print("Searching for LIKWID installation")
-    for p in os.environ["PATH"].split(":"):
-        cmd = "find %s/.. -type f -name \"liblikwid.so*\" 2>&1 | grep \"lib/\" | head -n1" % (p,)
-        try:
-            ps = subprocess.Popen(cmd, shell=True, close_fds=True, stdout=subprocess.PIPE)
-            sout, serr = ps.communicate()
-            if sout:
-                if len(sout) > 0:
-                    path = b"/".join(os.path.normpath(sout.strip()).split(b"/")[:-2])
-                    break
-        except:
-            pass
-    print("Using LIKWID libray at {!s}".format(path.decode()))
-    return bytes(path).decode().strip("\n").encode()
-
-def get_lib_path(prefix):
-    path = (prefix.decode()+"/lib").encode()
-    lib = "likwid"
-    cmd = "find %s -type f -name \"liblikwid.so*\" 2>&1 | sort | tail -n 1" % (prefix.decode(),)
-    try:
-        ps = subprocess.Popen(cmd, shell=True, close_fds=True, stdout=subprocess.PIPE)
-        sout, serr = ps.communicate()
-
-        if sout:
-            inp = sout.decode().strip("\n").encode()
-            path = os.path.dirname(inp)
-            lib = b":"+os.path.basename(inp)
-    except:
-        pass
-    return path, lib
-
-def get_inc_path(prefix):
-    path = (prefix.decode()+"/include").encode()
-    cmd = "find %s -type f -name \"likwid.h\" 2>&1 | sort | tail -n 1" % (prefix.decode(),)
-    try:
-        ps = subprocess.Popen(cmd, shell=True, close_fds=True, stdout=subprocess.PIPE)
-        sout, serr = ps.communicate()
-        if sout:
-            path = os.path.dirname(sout.decode().strip("\n").encode())
-    except:
-        pass
+    path_to_explore = None
+    is_searching = True
+    if path_iterator:
+        while is_searching:
+            try:
+                path_to_explore = next(path_iterator)
+                if os.path.exists(path_to_explore):
+                    path = path_to_explore
+                is_searching = False
+            except StopIteration:
+                is_searching = False
     return path
 
 
-LIKWID_PREFIX = get_prefix()
-if not LIKWID_PREFIX:
-    LIKWID_PREFIX = str(DEF_LIKWID_PREFIX)
-LIKWID_LIBPATH, LIKWID_LIB = get_lib_path(LIKWID_PREFIX)
-LIKWID_INCPATH = get_inc_path(LIKWID_PREFIX)
+def get_hierarchy():
+    print("Searching for LIKWID installation")
+    include_path = None
+    library_path = None
+    library = None
+    prefix = os.getenv('LIKWID_PREFIX', None)
+    library_pattern = 'lib*/liblikwid.so*'
+    if prefix is not None:
+        iterator = glob.iglob(prefix + library_pattern, recursive=True)
+        library = find_file(iterator)
+        if library is not None:
+            library_path = os.path.dirname(library)
+    else:
+        paths_iterator = iter(os.environ["PATH"].split(":"))
+        is_searching = True
+        while is_searching:
+            try:
+                path = next(paths_iterator)
+                prefix = os.path.abspath(path + '../')
+                iterator = glob.iglob(prefix + library_pattern,
+                                      recursive=True)
+                library = find_file(iterator)
+                if library is not None:
+                    library_path = os.path.dirname(library)
+                    is_searching = False
+            except StopIteration:
+                is_searching = False
+            if library_path is None:
+                prefix = '/usr/local'
+                iterator = glob.iglob(prefix + library_pattern,
+                                      recursive=True)
+                library = find_file(iterator)
+                if library is not None:
+                    library_path = os.path.dirname(library)
+                    is_searching = False
+
+    include_path = os.path.join(prefix, 'include')
+    if not os.path.exists(os.path.join(include_path, 'likwid.h')):
+        iterator = glob.iglob(prefix + '**/likwid.h', recursive=True)
+        include_path = find_file(iterator)
+    if prefix is None or not os.path.exists(prefix):
+        raise Exception('Error the likwid prefix directory was not found')
+    if library is None or not os.path.exists(library):
+        raise Exception('Error the likwid library was not found')
+    if library_path is None or not os.path.exists(library_path):
+        raise Exception('Error the likwid library directory was not found')
+    if include_path is None or not os.path.exists(include_path):
+        raise Exception('Error the likwid include directory was not found')
+    print("Using LIKWID library at {!s}".format(library_path))
+    return prefix, library_path, library, include_path
+
+
+LIKWID_PREFIX, LIKWID_LIBPATH, LIKWID_LIB, LIKWID_INCPATH = get_hierarchy()
 
 pylikwid = Extension("pylikwid",
-                    include_dirs = [LIKWID_INCPATH.decode()],
-                    libraries = [LIKWID_LIB.decode()],
-                    #libraies = ["likwid"] links with liblikwid.so
-                    #libraies = [":liblikwid.so.4.3"] links with liblikwid.so.4.3
-                    library_dirs = [LIKWID_LIBPATH.decode()],
-                    sources = ["pylikwid.c"])
+                     include_dirs=[LIKWID_INCPATH],
+                     libraries=[LIKWID_LIB],
+                     library_dirs=[LIKWID_LIBPATH],
+                     sources=["pylikwid.c"])
 
 setup(
-    name = "pylikwid",
-    version = "0.2.9",
-    author = "Thomas Roehl",
-    author_email = "thomas.roehl@googlemail.com",
-    description = ("A Python module to access the function of the LIKWID library"),
+    name="pylikwid",
+    version="0.2.9",
+    author="Thomas Roehl",
+    author_email="thomas.roehl@googlemail.com",
+    description="A Python module to access the function of the LIKWID library",
     long_description=read("README.rst"),
-    license = "GPLv2",
-    keywords = "hpc performance benchmark analysis",
-    url = "https://github.com/RRZE-HPC/pylikwid",
+    license="GPLv2",
+    keywords="hpc performance benchmark analysis",
+    url="https://github.com/RRZE-HPC/pylikwid",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
@@ -98,9 +114,9 @@ setup(
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
     ],
-    package_data = {
-        "pylikwid" : ["pylikwid.c", "README.rst", "LICENSE"],
-        "tests" : ["tests/*.py"]
+    package_data={
+        "pylikwid": ["pylikwid.c", "README.rst", "LICENSE"],
+        "tests": ["tests/*.py"]
     },
-    ext_modules = [pylikwid]
+    ext_modules=[pylikwid]
 )
