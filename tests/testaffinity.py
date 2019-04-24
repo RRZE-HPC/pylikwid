e49cec7dafd0bdd5491b85d3460d759a3203c74e
Thomas Roehl <thomas.roehl@fau.de>
2019-04-24 16:12:12 +0200

    Update for testaffinity.py

--- tests/testaffinity.py
+++ tests/testaffinity.py
@@ -3,19 +3,21 @@
 import pylikwid
 
 def int_join(l, sep):
-    return sep.join([str(x) for x in l])
+    if l and len(l) > 0:
+        return sep.join([str(x) for x in l])
+    return "\"\""
 
 aff = pylikwid.initaffinity()
 
 for k in aff:
     if isinstance(aff[k], int):
         print("{}: {}".format(k, aff[k]))
-print()
+print("")
 
 for d in aff["domains"]:
     print("Domain {}:".format(aff["domains"][d]["tag"]))
     print("\t"+int_join(aff["domains"][d]["processorList"], " "))
-    print()
+    print("")
 
 for sel in ["S0:0-3", "N:3-1", "1,2,3", "E:N:2:1:2"]:
     l = pylikwid.cpustr_to_cpulist(sel)
